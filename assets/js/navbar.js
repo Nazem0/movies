772c377a28020fa6f9bd9dc01e9d0e7b2a8b6bf6
Nazem0 <109486038+Nazem0@users.noreply.github.com>
2023-07-23 15:47:54 +0300

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,41 +1,18 @@
-document.body.insertAdjacentHTML('afterbegin',`<body>
-<style>
-    .navbar {
-background-color: rgba(0, 0, 0, 0.3);
-overflow: hidden;
-position: fixed;
-top: 0;
-width: 100%;
-}
-
-/* Style the links inside the navigation bar */
-.navbar a {
-float: left;
-color: #f2f2f2;
-text-align: center;
-padding: 14px 16px;
-text-decoration: none;
-font-size: 17px;
-transition: 0.5s;
-
-}
-
-/* Change the color of links on hover */
-.navbar a:hover {
-background-color: #ddd;
-color: black;
-}
-
-/* Add a color to the active/current link */
-.topnav a.active {
-background-color: #04AA6D;
-color: white;
-}
-</style>
-<div class="navbar">
-    <a class="active" href="#home">Home<img src="assets/data/house-solid.svg" alt=""></a>
-    <a href="#news">News</a>
-    <a href="#contact">Contact</a>
-    <a href="#about">help</a>
-</div>
-</body>`)
+document.body.insertAdjacentHTML('afterbegin',
+            `<div class="navbar">
+                <ul type="none">
+                    <li><a class="active" href="#home">Home<img src="assets/data/house-solid.svg" alt=""></a></li>
+                    <li><a href="#news">News</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+                <div id="filtering">
+                        <input type="text" name="search" id="search" placeholder="Search By Title">
+                        <select name="genre" id="genreFilter">
+                            <option value="">Filter</option>
+                        </select>
+                    </div>
+                    <ul type="none">
+                    <li><a href="#news">Register</a></li>
+                    <li><a href="#contact">Login</a></li>
+                </ul>
+            </div>`)
