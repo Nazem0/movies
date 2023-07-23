685d6f287ac8c769763344a0004328703e200fa0
Samer Said <supsamer2009@icloud.com>
2023-07-23 16:14:35 +0300

    register

--- assets/js/reg.js
+++ assets/js/reg.js
@@ -3,25 +3,25 @@ var email;
 var user = {};
 
 function checkLocalStorage() {
-    var local =[];
+    var local = [];
     username = document.getElementById('username').value;
+    // +1 34an m4 by5o4 el loop law mafe4 users
     for (var i = 0; i < localStorage.length + 1; i++) {
         var key = localStorage.key(i)
         // console.log('local : ' + localStorage.getItem(i));
         // console.log(`${key}: ${localStorage.getItem(key)}`);
         local[i] = key;
     }
-    console.log(local);
+    if (local.indexOf(username) > -1) {
+        //console.log('1')
+        alert('Username is no available!');
+    } else {
+        // console.log('2')
+        register();
+    }
+    //console.log(local);
 }
-// if (username != key) {
-//     console.log('1')
-//     register();
 
-// } else {
-//     console.log('2')
-//     alert('Username is no available!');
-    
-// }
 function register() {
     username = document.getElementById('username').value;
     email = document.getElementById('email').value;
@@ -33,5 +33,5 @@ function register() {
     };
     var json = JSON.stringify(user);
     localStorage.setItem(username, json);
-    console.log('the register user is: ' + json);
+   // console.log('the register user is: ' + json);
 }
