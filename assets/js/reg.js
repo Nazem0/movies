1d3f020a8edb3cb4a8e091b26d25d8db5fc7af96
Samer Said <supsamer2009@icloud.com>
2023-07-23 12:01:41 +0300

    login and register

---
+++ assets/js/reg.js
@@ -0,0 +1,10 @@
+var username;
+var email;
+var password;
+function register(){
+    username = document.getElementById('username');
+    email = document.getElementById('email');
+    password = document.getElementById('password');
+
+    
+}
