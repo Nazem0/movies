7192c98431ca6d21fe19dd7cb2d7335be1665479
Samer Said <supsamer2009@icloud.com>
2023-07-27 06:54:35 +0300

    login update

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,30 +1,38 @@
-var email;
+var emailLogin;
+var userLogin;
 var check;
 oldDB = JSON.parse(localStorage.getItem('users')) || [];
 
 
 function checkLocalStorageLogin() {
-    var localEmailLogin = [];
-    var LocalPassword = [];
-    var email = document.getElementById('emailLogin').value;
-    var password = document.getElementById('passwordLogin').value;
+    emailLogin = document.getElementById('emailLogin').value;
+    var passwordLogin = document.getElementById('passwordLogin').value;
 
     for (let i = 0; i < oldDB.length; i++) {
-        if (oldDB[i].email == email && oldDB[i].password == password) {
+        if (oldDB[i].email == emailLogin && oldDB[i].password == passwordLogin) {
             check = 0;
-            login();
+            login(oldDB[i].username);
             break;
         } else {
             check = 1;
         }
     }
-    if(check == 1){
+    if (check == 1) {
         alert('Email or password are not incorrect!');
     }
 }
 
-function login() {
-    email = document.getElementById('emailLogin').value;
-    sessionStorage.setItem('email', email);
+function login(user) {
+    userLogin = user;
+    emailLogin = document.getElementById('emailLogin').value;
+    userObj = { 'email': emailLogin, 'user': userLogin }
+
+    var userJson = JSON.stringify(userObj);
+    sessionStorage.setItem('currentUser', userJson);
+}
+
+function getCurrentUser() {
+    var currentJson = JSON.parse(sessionStorage.getItem('currentUser'));
+    return currentJson.user;
 }
 
