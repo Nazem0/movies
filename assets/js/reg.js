84008a29ea00884d4999283957d7f71a470b2e23
Nazem0 <Mohamednazem09@gmail.com>
2023-07-28 14:22:09 +0300

    username to fullName

--- assets/js/reg.js
+++ assets/js/reg.js
@@ -35,3 +35,42 @@ function register() {
     var json = JSON.stringify(users);
     localStorage.setItem('users', json);
 }
+
+
+// var username;
+// var email;
+// var user = {};
+// var oldDB = JSON.parse(localStorage.getItem('users'));
+
+// function checkLocalStorage() {
+//     var localUser = [];
+//     var localEmail = [];
+//     username = document.getElementById('username').value;
+//     email = document.getElementById('email').value;
+//     for (var i = 0; i < oldDB.length; i++) {
+//         localUser[i] = oldDB[i].username;
+//     }
+//     if (localUser.indexOf(username) || localEmail.indexOf(email) > -1) {
+//         alert('Username or Email are no available!');
+//     } else {
+//         register();
+//     }
+// }
+
+// function register() {
+//     var users = [];
+//     if (oldDB) {
+//         users = oldDB;
+//     }
+//     username = document.getElementById('username').value;
+//     email = document.getElementById('email').value;
+//     var password = document.getElementById('password').value;
+//     user = {
+//         username: username,
+//         email: email,
+//         password: password,
+//     };
+//     users.push(user);
+//     var json = JSON.stringify(users);
+//     localStorage.setItem('users', json);
+// }
