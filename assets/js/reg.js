8ed63eed8e36cdf5312b1502b90ad758fea5062e
nazem0 <mohamednazem09@gmail.com>
2023-07-30 16:20:34 +0200

    UPDATED REGISTER NOW VALIDATES THE COMING INPUT AND DISPLAY ERROR MESSAGE

--- assets/js/reg.js
+++ assets/js/reg.js
@@ -1,57 +1,18 @@
-var fullName;
-var email;
-var user = {};
-var oldDB = [];
-oldDB = JSON.parse(localStorage.getItem('users'))||[];
-console.log(oldDB);
-
-function checkLocalStorage() {
-    var localEmail = [];
-    email = document.getElementById('email').value.toLowerCase();
-    for (var i = 0; i < oldDB.length; i++) {
-        localEmail[i] = oldDB[i].email;
-    }
-    if (localEmail.indexOf(email) > -1) {
-        alert('Username or Email are not available!');
-    } else {
-        register();
-    }
-}
-
-function register() {
-    var users = [];
-    if (oldDB) {
-        users = oldDB;
-    }
-    fullName = document.getElementById('fullName').value;
-    email = document.getElementById('email').value.toLowerCase();
-    let password = document.getElementById('password').value;
-    user = {
-        fullName: fullName,
-        email: email,
-        password: password,
-    };
-    users.push(user);
-    var json = JSON.stringify(users);
-    localStorage.setItem('users', json);
-}
-
-
-// var username;
+// var fullName;
 // var email;
 // var user = {};
-// var oldDB = JSON.parse(localStorage.getItem('users'));
+// var oldDB = [];
+// oldDB = JSON.parse(localStorage.getItem('users'))||[];
+// console.log(oldDB);
 
 // function checkLocalStorage() {
-//     var localUser = [];
 //     var localEmail = [];
-//     username = document.getElementById('username').value;
-//     email = document.getElementById('email').value;
+//     email = document.getElementById('email').value.toLowerCase();
 //     for (var i = 0; i < oldDB.length; i++) {
-//         localUser[i] = oldDB[i].username;
+//         localEmail[i] = oldDB[i].email;
 //     }
-//     if (localUser.indexOf(username) || localEmail.indexOf(email) > -1) {
-//         alert('Username or Email are no available!');
+//     if (localEmail.indexOf(email) > -1) {
+//         alert('Username or Email are not available!');
 //     } else {
 //         register();
 //     }
@@ -62,11 +23,11 @@ function register() {
 //     if (oldDB) {
 //         users = oldDB;
 //     }
-//     username = document.getElementById('username').value;
-//     email = document.getElementById('email').value;
-//     var password = document.getElementById('password').value;
+//     fullName = document.getElementById('fullName').value;
+//     email = document.getElementById('email').value.toLowerCase();
+//     let password = document.getElementById('password').value;
 //     user = {
-//         username: username,
+//         fullName: fullName,
 //         email: email,
 //         password: password,
 //     };
@@ -74,3 +35,48 @@ function register() {
 //     var json = JSON.stringify(users);
 //     localStorage.setItem('users', json);
 // }
+const emailRE = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const nameRE = /^[A-Za-z]+$/;
+var user = {};
+var oldDB = [];
+oldDB = JSON.parse(localStorage.getItem('users')) || [];
+
+function checkLocalStorage() {
+    let fullName = document.getElementById('fullName').value;
+    let email = document.getElementById('email').value.toLowerCase();
+    let password = document.getElementById('password').value;
+    if (nameRE.test(fullName)&&emailRE.test(email)&&password.length>=8) {
+        if(document.getElementById('warning'))
+        var localEmail = [];
+        email = document.getElementById('email').value.toLowerCase();
+        for (var i = 0; i < oldDB.length; i++) {
+            localEmail[i] = oldDB[i].email;
+        }
+        if (localEmail.indexOf(email) > -1) {
+            alert('Username or Email are not available!');
+        } else {
+            register(fullName, email, password);
+        }
+    }
+    else
+    {
+        if(!document.getElementById('warning'))
+        document.body.insertAdjacentHTML('afterbegin',`<div id="warning"><h2>Invalid Input</h2>
+        <p>Please make sure that your name consists of characters only, your email is valid, and your password length is not less than 8.</p></div>`)
+    }
+}
+
+function register(fullName, email, password) {
+    var users = [];
+    if (oldDB) {
+        users = oldDB;
+    }
+    user = {
+        fullName: fullName,
+        email: email,
+        password: password,
+    };
+    users.push(user);
+    var json = JSON.stringify(users);
+    localStorage.setItem('users', json);
+}
